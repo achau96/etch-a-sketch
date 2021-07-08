1f8e0a8114a05da628291ab9e5388b00045a301a
Austin Chau <austin.chau1@gmail.com>
2021-07-08 18:52:16 -0400

    completed etch-a-sketch

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # etch-a-sketch
+
+Objectives:
+1. Testing DOM manipulation skills
+2. Testing basic Javascript/html/css skills
+3. Testing google skills! Hell yeah!
---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container')
+const btn = document.querySelector('#btn')
+
+const changeColor = (e) => {
+  let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+e.target.style.background = randomColor;
+}
+
+const getBoxSize = () => {
+  let boxSize = prompt("Please enter N for new box size NxN");
+  container.textContent = "";
+  for(let i = 0; i<(boxSize*boxSize); i++){
+    const div = document.createElement('div');
+    div.setAttribute('class','blocks')
+    div.setAttribute('style', 'aspect-ratio:1;border-style:solid;border-width:1px');
+    container.setAttribute('style', `grid-template-columns: repeat(${boxSize},1fr);grid-template-rows: repeat(${boxSize},1fr)`)
+    container.appendChild(div);
+    }
+    const divs = document.querySelectorAll('.blocks'); //obtains all block elements
+  divs.forEach(div => {
+  div.addEventListener('mouseover', changeColor)
+})
+}
+
+btn.addEventListener('click',getBoxSize)
+
+for(let i = 0; i<(16*16); i++){
+const div = document.createElement('div');
+div.setAttribute('class','blocks')
+div.setAttribute('style', 'aspect-ratio:1;border-style:solid;border-width:1px');
+container.setAttribute('style', `grid-template-columns: repeat(16,1fr);grid-template-rows: repeat(16,1fr)`)
+container.appendChild(div);
+}
+
+const divs = document.querySelectorAll('.blocks'); //obtains all block elements
+divs.forEach(div => {
+  div.addEventListener('mouseover', changeColor)
+})
